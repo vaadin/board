5e07b35a3ae21d8f7cebf05e73b1aca204ce5b01
Sven Ruppert <sven.ruppert@gmail.com>
2017-04-22 14:42:56 +0300

    added bootstrap servlets / UI

---
+++ integration-tests/src/main/java/org/rapidpm/vaadin/addon/board/bootstrap/core/DDIVaadinServlet.java
@@ -0,0 +1,42 @@
+package org.rapidpm.vaadin.addon.board.bootstrap.core;
+
+import java.util.List;
+
+import javax.servlet.ServletException;
+
+import com.vaadin.server.DeploymentConfiguration;
+import com.vaadin.server.ServiceException;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.server.VaadinServletService;
+
+/**
+ * Created by svenruppert on 06.04.17.
+ */
+public abstract class DDIVaadinServlet extends VaadinServlet {
+
+    @Override
+    protected void servletInitialized()
+        throws ServletException {
+        super.servletInitialized();
+
+    }
+
+    //add Metrics here
+
+    @Override
+    protected VaadinServletService createServletService(
+        final DeploymentConfiguration deploymentConfiguration)
+        throws ServiceException {
+        final DDIVaadinServletService service = new DDIVaadinServletService(
+            this, deploymentConfiguration, topLevelPackagesToActivate());
+        service.init();
+        return service;
+    }
+
+    /**
+     * return a list of pkg names that are available for Injection
+     *
+     * @return return
+     */
+    public abstract List<String> topLevelPackagesToActivate();
+}
