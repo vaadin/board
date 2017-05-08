25615311748f1e2884a7349a94af701eb7f26ab2
rogozinds <rogozinds@gmail.com>
2017-05-08 12:16:17 +0300

    Add RemoveComponent test

---
+++ integration-tests/src/test/java/com/vaadin/addon/board/testbenchtests/AddRemoveRowIT.java
@@ -0,0 +1,29 @@
+package com.vaadin.addon.board.testbenchtests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.addon.board.testUI.AddRemoveRowUI;
+import com.vaadin.testbench.By;
+import com.vaadin.testbench.elements.ButtonElement;
+
+public class AddRemoveRowIT extends AbstractParallelTest {
+
+    @Override
+    protected Class<?> getUIClass() {
+        return AddRemoveRowUI.class;
+    }
+
+    @Test
+    public void basicLayout_boardTabletSize_twoRowsAndTwoItemsInRow() {
+        WebElement board =getDriver().findElement(By.tagName("vaadin-board"));
+        ButtonElement btn1 = $(ButtonElement.class).caption("Button 1").first();
+        ButtonElement btn2 = $(ButtonElement.class).caption("Button 3").first();
+
+        Assert.assertEquals("Btn1 should have Y == 0" , board.getLocation().getY(), btn1.getLocation().getY());
+        Assert.assertEquals("Btn2 should have Y == 0", board.getLocation().getY(), btn2.getLocation().getY());
+
+    }
+
+}
