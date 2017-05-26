a879e5a0f840940d87db81a4f861cda324b87487
Sven Ruppert <sven.ruppert@gmail.com>
2017-05-26 08:33:17 +0200

    [DASH-27] - added generic UI Test

--- integration-tests/src/test/java/com/vaadin/addon/board/testbenchtests/Dash27HorizontalLayoutUIIT.java
+++ integration-tests/src/test/java/com/vaadin/addon/board/testbenchtests/Dash27HorizontalLayoutUIIT.java
@@ -1,21 +1,45 @@
 package com.vaadin.addon.board.testbenchtests;
 
-import org.junit.Test;
+import static com.vaadin.addon.board.testUI.UIFunctions.ID_PREFIX;
+import static com.vaadin.addon.board.testUI.UIFunctions.SWITCH;
+import static com.vaadin.addon.board.testbenchtests.TestFunctions.genericAssertHeight;
+import static com.vaadin.addon.board.testbenchtests.TestFunctions.genericAssertWidth;
 
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
 import com.vaadin.addon.board.testUI.Dash27LayoutComponents;
+import com.vaadin.testbench.elements.ButtonElement;
 
 /**
  *
  */
 public class Dash27HorizontalLayoutUIIT extends AbstractParallelTest {
-    @Override
-    protected Class<?> getUIClass() {
-        return Dash27LayoutComponents.HorizontalLayoutUI.class;
-    }
+  @Override
+  protected Class<?> getUIClass() {
+    return Dash27LayoutComponents.HorizontalLayoutUI.class;
+  }
 
-    @Test
-    public void test001()
-        throws Exception {
-//        testBench(getDriver()).
-    }
+
+  Supplier<WebElement> buttonMiddleSupplier = () -> $(ButtonElement.class)
+      .id(ID_PREFIX + 2);
+
+  @Test
+  public void testGenericWidth()
+      throws Exception {
+    genericAssertWidth.accept(buttonSwitchSupplier, buttonMiddleSupplier);
+  }
+
+  @Test
+  public void testGenericHeight()
+      throws Exception {
+    genericAssertHeight.accept(buttonSwitchSupplier, buttonMiddleSupplier);
+  }
+
+
+
 }
