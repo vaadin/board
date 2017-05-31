4f20b501a46c82ff693679513f695a37e5b30eb9
Sven Ruppert <sven.ruppert@gmail.com>
2017-05-31 11:03:25 +0200

    Feature/dash 27 one commit (#57)
    * Update vaadin version. Use viewport annotation in demo.
    
    * Merge branch 'master' of github.com:vaadin/board
    
    * Add docs for Java-API chapter, fixes DASH-30
    
    * Add Note about using @Viewport
    
    * Merge remote-tracking branch 'origin/master'
    
    # Conflicts:
    #	documentation/java-api/basic-use.asciidoc
    #	documentation/java-api/getting-started.asciidoc
    #	documentation/java-api/installing.asciidoc
    
    * [DASH-27] - one commit to change them all....
    
    * [DASH-27] - removed Supplier from Charts UI/Tests as recommended
    
    * [DASH-27] - removed Supplier from Charts UI/Tests as recommended

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn deploy --settings ./.travis.settings.xml -DrepositoryId=vaadin-snapshots -Dvaadin.board.developer.license=$BOARD_LICENSE -DaltDeploymentRepository=vaadin-snapshots::default::https://oss.sonatype.org/content/repositories/vaadin-snapshots/ -DskipTests=true -DskipITs -B
+mvn deploy --settings ./.travis.settings.xml -DrepositoryId=vaadin-snapshots -Dvaadin.spreadsheet.developer.license=$SPREADSHEET_LICENSE -Dvaadin.charts.developer.license=$CHARTS_LICENSE -Dvaadin.board.developer.license=$BOARD_LICENSE -DaltDeploymentRepository=vaadin-snapshots::default::https://oss.sonatype.org/content/repositories/vaadin-snapshots/ -DskipTests=true -DskipITs -B
--- .travis.install.sh
+++ .travis.install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn install -Dvaadin.board.developer.license=$BOARD_LICENSE -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -DskipTests=true -DskipITs
+mvn install -Dvaadin.spreadsheet.developer.license=$SPREADSHEET_LICENSE -Dvaadin.charts.developer.license=$CHARTS_LICENSE -Dvaadin.board.developer.license=$BOARD_LICENSE -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -DskipTests=true -DskipITs
